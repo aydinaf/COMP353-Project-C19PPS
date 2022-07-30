fb0d2bfc1f73782704f705e22b56251a45e0664c
Aydin Azari Farhad <33768239+aydinaf@users.noreply.github.com>
2022-07-29 23:58:13 -0400

    Modified several tables to fix errors
    Multiple tables were modified to fix any foreign keys referencing non-unique keys to fix errors.
    All queries execute with no errors. A review is still necessary to catch any possible discrepancies.
    
    Summary of modifications:
    > authorFirstName and authorLastName were removed and replaced with authorUsername.
    > Email under Users table was declared as a UNIQUE attribute.

--- ddl_queries.sql
+++ ddl_queries.sql
@@ -10,7 +10,7 @@ CREATE TABLE Users(
     firstName VARCHAR (255) NOT NULL, -- RESIZE LENGHTS OF VARCHARS
     lastName VARCHAR (255) NOT NULL,
     citizenship VARCHAR (255) NOT NULL,
-    email VARCHAR (255) NOT NULL,
+    email VARCHAR (255) UNIQUE NOT NULL,
     phone INT NOT NULL,
     pwdChecksum VARCHAR (255) NOT NULL,
     PRIMARY KEY (username)
@@ -26,16 +26,14 @@ CREATE TABLE Privleges (
 
 CREATE TABLE Articles (
 	articleName VARCHAR(255) NOT NULL,
-	authorFirstName VARCHAR(255),
-	authorLastName VARCHAR(255),
-	orgID INT(255),
+	authorUsername VARCHAR(255) NOT NULL,
+	orgID INT NOT NULL,
 	publicationDate DATE NOT NULL,
 	majorTopic VARCHAR(255) NOT NULL,
 	minorTopic VARCHAR(255) NOT NULL,
 	summary VARCHAR(255) NOT NULL,
-	PRIMARY KEY (articleName, authorFirstName, authorLastName, orgID, publicationDate),
-	FOREIGN KEY (authorFirstName) REFERENCES Users (firstName) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (authorLastName) REFERENCES Users (lastName) ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEY (articleName, authorUsername, orgID, publicationDate),
+	FOREIGN KEY (authorUsername) REFERENCES Users (username) ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY (orgID) REFERENCES Organizations (orgID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
@@ -50,12 +48,10 @@ CREATE TABLE EmailLogs (
 
 CREATE TABLE Subscriptions (
 	username VARCHAR (255) NOT NULL,
-    authorFirstName VARCHAR (255) NOT NULL,
-    authorLastName VARCHAR (255) NOT NULL,
+    authorUsername VARCHAR (255) NOT NULL,
     organization VARCHAR (255) NOT NULL,
     FOREIGN KEY (username) REFERENCES Users (username) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (authorFirstName) REFERENCES Articles (firstName) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (authorLastName) REFERENCES Articles (lastName) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (authorUsername) REFERENCES Articles (authorUsername) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Delegates (
@@ -88,7 +84,7 @@ CREATE TABLE ProStaTers (
 );
 
 CREATE TABLE Vaccines (
-	vaccineName VARCHAR (255) NOT NULL
+	vaccineName VARCHAR (255) NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE Vaccinations (
